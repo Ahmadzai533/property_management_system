1a5c807a94f1812a8a12ddae2cf6fc7ac195ab1f
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-06-30 21:03:48 +0430

    added

---
+++ src/pages/properties/LeasePropertyPages/PropertyStatusBadge.jsx
@@ -0,0 +1,13 @@
+import PropertyStatusBadge from "../../../components/properties/PropertyStatusBadge";
+
+const Properties = () => {
+  return (
+    <div className="p-6 flex gap-3">
+      <PropertyStatusBadge status="occupied" />
+      <PropertyStatusBadge status="vacant" />
+      <PropertyStatusBadge status="custom" />
+    </div>
+  );
+};
+
+export default Properties;
