89ee91f80da50a4325174b4dd9915b3b4d201ab4
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-06-30 21:04:35 +0430

    added

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,24 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "../components/layout/DashboardLayout";
-import Dashboard from "../pages/Dashboard";
+import Dashboard from "../Dashboard/Dashboard";
 import Properties from "../pages/Properties";
-import PropertyList from "../pages/properties/PropertyList";
-import OwnProperties from "../pages/properties/OwnProperties";
-import OwnPropertyDetails from "../pages/properties/OwnPropertyDetails";
-import AddOwnProperty from "../pages/properties/AddOwnProperty";
-import EditOwnProperty from "../pages/properties/EditOwnProperty";
-import LeaseProperties from "../pages/properties/LeaseProperties";
-import LeasePropertyDetails from "../pages/properties/LeasePropertyDetails";
-import AddLeaseProperty from "../pages/properties/AddLeaseProperty";
-import EditLeaseProperty from "../pages/properties/EditLeaseProperty";
-import AllUnits from "../pages/properties/AllUnits";
-import UnitDetails from "../pages/properties/UnitDetails";
-import AddUnit from "../pages/properties/AddUnit";
-import EditUnit from "../pages/properties/EditUnit";
+import PropertyList from "../pages/Properties/AllPropertyPages/PropertyList";
+import OwnProperties from "../pages/properties/OwnPropertyPages/OwnProperties";
+import OwnPropertyDetails from "../pages/properties/OwnPropertyPages/OwnPropertyDetails";
+import AddOwnProperty from "../pages/properties/OwnPropertyPages/AddOwnProperty";
+import EditOwnProperty from "../pages/properties/OwnPropertyPages/EditOwnProperty";
+import LeaseProperties from "../pages/properties/LeasePropertyPages/LeaseProperties";
+import LeasePropertyDetails from "../pages/properties/LeasePropertyPages/LeasePropertyDetails";
+import AddLeaseProperty from "../pages/properties/LeasePropertyPages/AddLeaseProperty";
+import EditLeaseProperty from "../pages/properties/LeasePropertyPages/EditLeaseProperty";
+import AllUnits from "../pages/Properties/AllUnitPages/AllUnits";
+import UnitDetails from "../pages/properties/AllUnitPages/UnitDetails";
+import AddUnit from "../pages/properties/AllUnitPages/AddUnit";
+import EditUnit from "../pages/properties/AllUnitPages/EditUnit";
+
+import PaymentsInvoices from "../pages/finance/PaymentsInvoices";
+import RentRollLedger from "../pages/finance/RentRollLedger";
+import TransactionHistory from "../pages/finance/TransactionHistory";
 
 const AppRoutes = () => {
   return (
@@ -28,19 +32,28 @@ const AppRoutes = () => {
         <Route path="properties/add" element={<AddOwnProperty />} />
         <Route path="properties/edit/:id" element={<EditOwnProperty />} />
         <Route path="properties/:id" element={<OwnPropertyDetails />} />
-        
+
         {/* Lease Properties Module */}
         <Route path="properties/lease" element={<LeaseProperties />} />
         <Route path="properties/lease/add" element={<AddLeaseProperty />} />
-        <Route path="properties/lease/edit/:id" element={<EditLeaseProperty />} />
+        <Route
+          path="properties/lease/edit/:id"
+          element={<EditLeaseProperty />}
+        />
         <Route path="properties/lease/:id" element={<LeasePropertyDetails />} />
-        
+
         {/* Units Module */}
         <Route path="properties/units" element={<AllUnits />} />
         <Route path="properties/units/add" element={<AddUnit />} />
         <Route path="properties/units/edit/:id" element={<EditUnit />} />
         <Route path="properties/units/:id" element={<UnitDetails />} />
-        
+
+        {/*Finance Module */}
+        {/* <Route path="/" element={<Navigate to="payments" replace />} /> */}
+        <Route path="finance/payments" element={<PaymentsInvoices />} />
+        <Route path="finance/ledger" element={<RentRollLedger />} />
+        <Route path="finance/transactions" element={<TransactionHistory />} />
+
         {/* Properties Module */}
         <Route path="properties/listed" element={<PropertyList />} />
         <Route path="properties/*" element={<Properties />} />
