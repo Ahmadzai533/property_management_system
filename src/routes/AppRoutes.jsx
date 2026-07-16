8e41933681a1119dd44b03ffab5fcd0604f94dcc
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-07-17 02:22:08 +0430

    Force update import casing for Vercel

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayout from "../components/layout/DashboardLayout";
 import Dashboard from "../Dashboard/Dashboard";
-import Properties from "../pages/Properties";
+import Properties from "../pages/properties";
 
 // Import Tenant Routes
 import TenantForm from "../pages/tenants/tanants";
